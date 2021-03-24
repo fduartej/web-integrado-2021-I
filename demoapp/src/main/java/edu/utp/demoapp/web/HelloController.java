6caadcbf681813de0e8e2669a09b4e74fc576d9c
Fred Duarte <gestion_atlassian@inteligogroup.com>
2021-03-23 20:03:40 -0500

    Hola mundo controller

--- demoapp/src/main/java/edu/utp/demoapp/web/HelloController.java
+++ demoapp/src/main/java/edu/utp/demoapp/web/HelloController.java
@@ -2,13 +2,15 @@ package edu.utp.demoapp.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class HelloController {
     
     @GetMapping("/saludo")
-    public String saludo(){
-        return "Hola desde un controller";
+    public String saludo(Model model){
+        model.addAttribute("name", "Hola desde un controller");
+        return "saludo";
     }
 
 }
