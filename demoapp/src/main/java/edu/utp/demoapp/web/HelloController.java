a31a17c0d5a5dcacb22c807fd2a87f9c0214c486
Fred Duarte <gestion_atlassian@inteligogroup.com>
2021-03-25 19:20:20 -0500

    Explicando MVC

--- demoapp/src/main/java/edu/utp/demoapp/web/HelloController.java
+++ demoapp/src/main/java/edu/utp/demoapp/web/HelloController.java
@@ -3,14 +3,19 @@ package edu.utp.demoapp.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import edu.utp.demoapp.model.Persona;
 
 @Controller
 public class HelloController {
     
     @GetMapping("/saludo")
-    public String saludo(Model model){
+    public String mimetodo(Model model){
+        Persona p = new Persona();
+        p.setFirstName("Bryan");
+        p.setLastName("Romero");
+        model.addAttribute("persona", p);
         model.addAttribute("name", "Hola desde un controller");
-        return "saludo";
+        return "mypagina";
     }
 
 }
