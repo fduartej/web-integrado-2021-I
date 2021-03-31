1e4f954f894fe4d2ff7823784a9c2346b0a25dfe
Fred Duarte <gestion_atlassian@inteligogroup.com>
2021-03-30 19:28:51 -0500

    Agregando Form y Dependencias

--- demoapp-form/src/main/java/edu/utp/demoappform/web/ContactController.java
+++ demoapp-form/src/main/java/edu/utp/demoappform/web/ContactController.java
@@ -1,5 +1,20 @@
 package edu.utp.demoappform.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class ContactController {
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact/index";
+    }
+
+    @PostMapping("/contact/create")
+    public String postSubmitForm(){
+        return "contact/index";
+    }
     
 }
