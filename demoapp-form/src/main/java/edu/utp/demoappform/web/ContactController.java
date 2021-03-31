1570970f9e04e13be0ee718e4f8ef2cd75169542
Fred Duarte <gestion_atlassian@inteligogroup.com>
2021-03-30 19:43:54 -0500

    Flujo de Formulario

--- demoapp-form/src/main/java/edu/utp/demoappform/web/ContactController.java
+++ demoapp-form/src/main/java/edu/utp/demoappform/web/ContactController.java
@@ -3,17 +3,23 @@ package edu.utp.demoappform.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+
+import edu.utp.demoappform.model.Contact;
 
 @Controller
 public class ContactController {
 
     @GetMapping("/contact")
-    public String contact(){
+    public String contact(Model model){
+        Contact contact = new Contact();
+        model.addAttribute("contact", contact);
         return "contact/index";
     }
 
     @PostMapping("/contact/create")
-    public String postSubmitForm(){
+    public String postSubmitForm(Model model ){
+        model.addAttribute("mensaje", "Se registro un contacto");
         return "contact/index";
     }
     
